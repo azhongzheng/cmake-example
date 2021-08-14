1beddecbcf1ba1738fdd812a6e9c81257179b355
azhong <zczwzw@163.com>
2021-08-14 09:25:01 +0800

    unit-testing-google-test-download 1

---
+++ 01-basic/H-third-party-library/spdlogproject/build/CMakeFiles/cpplog.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpplog.dir/main.cpp.o"
+  "CMakeFiles/cpplog.dir/main.cpp.o.d"
+  "cpplog"
+  "cpplog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpplog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
